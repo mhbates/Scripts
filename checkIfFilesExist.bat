b0bda14a83a12e8db95ba0cefb5a4ec4225fb593
Matthew Hathaway-Bates <matt@mhbates.com>
2020-06-25 16:20:06 -0700

    Folder ending backslashes should be in variables, not the code itself.

--- checkIfFilesExist.bat
+++ checkIfFilesExist.bat
@@ -4,6 +4,7 @@ rem We need this set to use variables within for loops
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 rem Set folder locations here
+rem Must include following backslash at the end of the folder path
 SET stagingUnprocessedFolder=""
 SET stagingProbableDuplicatesFolder=""
 SET stagingProbableUniquesFolder=""
@@ -11,17 +12,17 @@ SET folderToSearchForExisting=""
 
 rem For each file in Unprocessed folder, search for it and move to appropriate subfolder
 rem If duplicate, put in duplicates folder; if not, put in uniques folder
-FOR /F %%G IN ('dir /b %stagingUnprocessedFolder%\') DO (
+FOR /F %%G IN ('dir /b %stagingUnprocessedFolder%') DO (
 	SET fileName=%%G
-	SET search="dir /S !folderToSearchForExisting!\!fileName! | findstr !fileName! | find /c /v "" "
+	SET search="dir /S !folderToSearchForExisting!!fileName! | findstr !fileName! | find /c /v "" "
 	FOR /F "tokens=*" %%i IN (' !search! ') DO SET X=%%i
 	IF !X! GEQ 2 (
 		echo Probable duplicate, moving file to duplicates folder - !fileName!
-		MOVE /-Y !stagingUnprocessedFolder!\!fileName! !stagingProbableDuplicatesFolder!\
+		MOVE /-Y !stagingUnprocessedFolder!!fileName! !stagingProbableDuplicatesFolder!
 	)
 	ELSE (
 		echo Probable unique, moving file to uniques folder - !fileName!
-		MOVE /-Y !stagingUnprocessedFolder!\!fileName! !stagingProbableUniquesFolder!\
+		MOVE /-Y !stagingUnprocessedFolder!!fileName! !stagingProbableUniquesFolder!
 	)
 )
 pause
