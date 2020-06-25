ce64d90459288fb9ce2d3375b2f8a4258bb382a7
M <matt@mhbates.com>
2020-06-25 15:09:03 -0700

    Initial upload

---
+++ checkIfFilesExist.bat
@@ -0,0 +1,27 @@
+@echo off
+
+rem We need this set to use variables within for loops
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+rem Set folder locations here
+SET stagingUnprocessedFolder=""
+SET stagingProbableDuplicatesFolder=""
+SET stagingProbableUniquesFolder=""
+SET folderToSearchForExisting=""
+
+rem For each file in Unprocessed folder, search for it and move to appropriate subfolder
+rem If duplicate, put in duplicates folder; if not, put in uniques folder
+FOR /F %%G IN ('dir /b %stagingUnprocessedFolder%\') DO (
+	SET fileName=%%G
+	SET search="dir /S !folderToSearchForExisting!\!fileName! | findstr !fileName! | find /c /v "" "
+	FOR /F "tokens=*" %%i IN (' !search! ') DO SET X=%%i
+	IF !X! GEQ 2 (
+		echo Probable duplicate, moving file to duplicates folder - !fileName!
+		MOVE /-Y !stagingUnprocessedFolder!\!fileName! !stagingProbableDuplicatesFolder!\
+	)
+	ELSE (
+		echo Probable unique, moving file to uniques folder - !fileName!
+		MOVE /-Y !stagingUnprocessedFolder!\!fileName! !stagingProbableUniquesFolder!\
+	)
+)
+pause
